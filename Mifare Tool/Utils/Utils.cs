edab764ad97bc9cb0ccd7347017bfdb3f1b8fb55
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2017-10-02 09:28:27 +0200

    Add utilities module and resource loader

---
+++ Mifare Tool/Utils/Utils.cs
@@ -0,0 +1,18 @@
+﻿using Windows.ApplicationModel.Resources;
+
+namespace Mifare_Tool.Utils
+{
+    public class Utils
+    {
+        private static ResourceLoader _rLoader;
+        public static ResourceLoader rLoader
+        {
+            get
+            {
+                if (_rLoader == null)
+                    _rLoader = new ResourceLoader();
+                return _rLoader;
+            }
+        }
+    }
+}
