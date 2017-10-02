a86b0eea87418aa2fb1c3a54a906fe0f56e60e1f
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2017-10-02 09:29:06 +0200

    Add module to manage files

---
+++ Mifare Tool/Utils/FileManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Mifare_Tool.Utils
+{
+    class FileManager
+    {
+        public static async void ImportFile()
+        {
+            //Get files from database
+            FileOpenPicker file_picker = new FileOpenPicker();
+            file_picker.FileTypeFilter.Add("*");
+            var file = await file_picker.PickSingleFileAsync();
+            if (file == null)
+            {
+                Communicator.SendPopup("file_null_title", "file_null_body");
+                return;
+            }
+            await file.CopyAsync(ApplicationData.Current.LocalFolder);
+        }
+
+        public static async Task<IReadOnlyList<StorageFile>> ListFiles()
+        {
+            var folder = ApplicationData.Current.LocalFolder;
+            return await folder.GetFilesAsync();
+        }
+    }
+}
