9230415d62b64ca2382e488000ebb1072ccf77a4
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2017-10-02 09:28:47 +0200

    Add module to comunicate with user

---
+++ Mifare Tool/Utils/Communicator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace Mifare_Tool.Utils
+{
+    public class Communicator
+    {
+        public static void SendPopup(string title, string message)
+        {
+            MessageDialog popup = new MessageDialog(Utils.rLoader.GetString(message), Utils.rLoader.GetString(title));
+            popup.ShowAsync();
+        }
+    }
+}
