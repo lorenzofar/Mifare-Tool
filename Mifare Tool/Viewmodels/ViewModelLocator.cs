925eb4a27a9b1388002e3f11ff385800ff2dbdd5
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2017-10-02 09:20:18 +0200

    Set up MVVM and Template 10

---
+++ Mifare Tool/Viewmodels/ViewModelLocator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mifare_Tool.Viewmodels
+{
+    public class ViewModelLocator
+    {
+        private static MainViewModel _main;
+        public static MainViewModel main
+        {
+            get
+            {
+                if (_main == null)
+                    _main = new MainViewModel();
+                return _main;
+            }
+        }
+    }
+}
