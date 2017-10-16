2cbd3403bb5b0f49af050e99937e11410daa1752
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2017-10-16 10:05:03 +0200

    Add icon to show card status
    Added circle icon to show the card status and a converter to set its color to green or red

---
+++ Mifare Tool/Converters/StatusBrushConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Mifare_Tool.Converters
+{
+    public class StatusBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null || !(bool)value)
+                return App.Current.Resources["RedBrush"];
+            return App.Current.Resources["GreenBrush"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
